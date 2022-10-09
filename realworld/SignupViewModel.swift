c81986699639972b14b294d510ded5db4998774b
jayoonkoo <rnwkdbs12@gmail.com>
2022-10-09 22:55:44 +0900

    feat: signUp View Model submit signup

--- realworld/SignupViewModel.swift
+++ realworld/SignupViewModel.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+class SignupViewModel {
+    let baseUrl = "http://localhost:3065/user"
+    
+    func submitSignUp(email: String, nickname: String, password: String, completion: @escaping () -> Void) {
+        guard let url = URL(string: baseUrl) else {
+            print("url 생성 오류")
+            return
+        }
+        let request = makeRequest(url: url, method: "POST", body: [
+            "email": email,
+            "nickname": nickname,
+            "password": password
+        ])
+        
+        URLSession.shared.dataTask(with: request) { data, _, error in
+            if let error = error {
+                print("error :\(error)")
+                return
+            }
+            completion()
+        }
+    }
+}
