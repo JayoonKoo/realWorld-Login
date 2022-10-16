675ee88037494aacf3b318ec1384ea1534edfa66
jayoonkoo <rnwkdbs12@gmail.com>
2022-10-16 17:04:29 +0900

    feat: login error 추가

--- realworld/Erros.swift
+++ realworld/Erros.swift
@@ -7,8 +7,15 @@
 
 import Foundation
 
+let serverErrorMessage = "서버 에러 - 잠시 후 다시 시도해주세요"
 
 enum SignUpError: Error {
     case duple(String)
     case serverError
+}
+
+enum LoginError: Error {
+    case invalidateUser
+    case serverError
+    case decodingError
 }
