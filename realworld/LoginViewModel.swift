3d269fd7e64539a0c5af9feb84b5421c94b5b20e
jayoonkoo <rnwkdbs12@gmail.com>
2022-10-16 17:04:41 +0900

    feat: loginViewModel 추가

--- realworld/LoginViewModel.swift
+++ realworld/LoginViewModel.swift
@@ -6,3 +6,47 @@
 //
 
 import Foundation
+
+struct LoginRequest {
+    let email: String
+    let password: String
+}
+
+class LoginViewModel: Request {
+    
+    func login(request: LoginRequest, completion: @escaping (Result<UserModel, LoginError>) -> Void) {
+        guard let url = URL(string: "\(baseUrl)/user/login") else {
+            print("url 생성 오류")
+            return
+        }
+        
+        let request = makeRequest(url: url, method: "POST", body: [
+            "email": request.email,
+            "password": request.password
+        ])
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            DispatchQueue.main.async {
+                guard let response = response as? HTTPURLResponse, let data = data else {
+                    completion(.failure(.serverError))
+                    return
+                }
+                let statusCode = response.statusCode
+                if (200..<300).contains(statusCode) {
+                    do {
+                        let user = try JSONDecoder().decode(UserModel.self, from: data)
+                        completion(.success(user))
+                    } catch {
+                        print(error.localizedDescription)
+                        completion(.failure(.decodingError))
+                    }
+                } else if 401 == statusCode {
+                    completion(.failure(.invalidateUser))
+                } else {
+                    completion(.failure(.serverError))
+                }
+            }
+        }.resume()
+    }
+}
+
