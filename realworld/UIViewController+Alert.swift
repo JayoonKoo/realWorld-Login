3a671009e41f14cecf01b4eb87845f12dab61b29
jayoonkoo <rnwkdbs12@gmail.com>
2022-10-16 17:03:50 +0900

    feat: viewController alert util 추가

--- realworld/UIViewController+Alert.swift
+++ realworld/UIViewController+Alert.swift
@@ -5,4 +5,14 @@
 //  Created by 구자윤 on 2022/10/14.
 //
 
-import Foundation
+import UIKit
+
+extension UIViewController {
+    func presentAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let cancleAction = UIAlertAction(title: "확인", style: .cancel)
+        alert.addAction(cancleAction)
+        
+        self.present(alert, animated: false)
+    }
+}
